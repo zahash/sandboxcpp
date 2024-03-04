a0e6830239bfe4437447f8eea19ed66a0d7eb540
zahash <zahash.z@gmail.com>
2024-03-04 08:52:53 +0530

    move opengl code to its own file

---
+++ hellogl.h
@@ -0,0 +1,49 @@
+#ifndef SANDBOXCPP_GL
+#define SANDBOXCPP_GL
+
+#include <GLFW/glfw3.h>
+
+int glrun(void)
+{
+    GLFWwindow *window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClearColor(0.5, 0.2, 0.3, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2d(-0.5, -0.5);
+        glVertex2d(0.0, 0.5);
+        glVertex2d(0.5, -0.5);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+#endif
